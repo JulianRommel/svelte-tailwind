6bf2dbd26a6997e66290c579356ef321f1d5cdf3
JulianRommel <info@rommel.solutions>
2021-07-13 15:53:42 +0200

    added import style in Tailwind.svelte component

--- src/Components/Tailwind.svelte
+++ src/Components/Tailwind.svelte
@@ -1,7 +1,5 @@
 <style global>
-    @import 'tailwindcss/base';
-  
-    @import 'tailwindcss/components';
-  
-    @import 'tailwindcss/utilities';
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
   </style>
